b518a14a6e89389b1f8e9371523725259fa2671c
Brennan Beam <BrennanBeam@gmail.com>
2024-01-08 23:05:48 +0000

    read a b file and print to console

--- actions/breaches.go
+++ actions/breaches.go
@@ -1,7 +1,29 @@
 package actions
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
-func updateBFile() {
-	fmt.Println("nachos")
+func updateBFile(bfilePath string) {
+	file, err := os.Open(bfilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//close the file when we're done
+	defer file.Close()
+
+	//read the file line by line
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+
+		fmt.Printf("line: %s\n", scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
--- actions/breaches_test.go
+++ actions/breaches_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+const oneBreachBFile string = "/workspaces/cc-ras-runner/actions/testResources/DamBreachOverlapDem.b01"
+
 func TestBreaching(t *testing.T) {
-	updateBFile()
+	updateBFile(oneBreachBFile)
 }
