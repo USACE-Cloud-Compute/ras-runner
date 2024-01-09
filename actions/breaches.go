aa6fc83e6e589d58d668564526ee41bba66ad060
Brennan Beam <BrennanBeam@gmail.com>
2024-01-09 23:42:51 +0000

    search for the interesting lines and return them as string slices.

--- actions/breaches.go
+++ actions/breaches.go
@@ -3,14 +3,18 @@ package actions
 import (
 	"bufio"
 	"errors"
-	"fmt"
 	"log"
 	"os"
+	"strings"
+	"unicode"
 )
 
 const rowLengthCellSizeError string = "the row was not able to be divided evenly by the cell size without remainder. Ensure the b-file has not been modified outside of RAS"
+const breachDataHeader string = "Breach Data"
 
-func updateBFile(bfilePath string) {
+func getBreachRows(bfilePath string) [][]string {
+	var breachDataRows [][]string
+
 	file, err := os.Open(bfilePath)
 	if err != nil {
 		log.Fatal(err)
@@ -22,13 +26,27 @@ func updateBFile(bfilePath string) {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-
-		fmt.Printf("line: %s\n", scanner.Text())
+		if strings.Contains(scanner.Text(), breachDataHeader) {
+			scanner.Scan()                                 //next line
+			var firstLetter rune = rune(scanner.Text()[0]) //first letter as a rune / kinda like a char.
+			for !unicode.IsLetter(firstLetter) {           // until we hit another header, keep going
+				row, err := splitRowsIntoCells(scanner.Text(), 8)
+				if err != nil {
+					log.Fatal(err)
+					return nil
+				}
+				breachDataRows = append(breachDataRows, row)
+				scanner.Scan()
+			}
+			if breachDataRows != nil {
+				break
+			}
+		}
 	}
-
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	return breachDataRows
 }
 
 // The b file is formated into columns 8 characters wide.
