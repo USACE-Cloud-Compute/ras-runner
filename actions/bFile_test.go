a728a635c8eeff036ee0a9990068c5e1bb9e6f18
Brennan Beam <BrennanBeam@gmail.com>
2024-02-01 20:35:04 +0000

    rename TestData to testData

--- actions/bFile_test.go
+++ actions/bFile_test.go
@@ -9,9 +9,9 @@ import (
 	"github.com/usace/cc-go-sdk"
 )
 
-const ONE_BREACH_FILE string = "/workspaces/cc-ras-runner/TestData/DamBreachOverlapDem.b01"
-const MULTI_BREACH_FILE string = "/workspaces/cc-ras-runner/TestData/multiDamBreach.b01"
-const FRAG_CURVE_PATH string = "/workspaces/cc-ras-runner/TestData/testFragilityCurveOutput.json"
+const ONE_BREACH_FILE string = "/workspaces/cc-ras-runner/testData/DamBreachOverlapDem.b01"
+const MULTI_BREACH_FILE string = "/workspaces/cc-ras-runner/testData/multiDamBreach.b01"
+const FRAG_CURVE_PATH string = "/workspaces/cc-ras-runner/testData/testFragilityCurveOutput.json"
 
 func TestWrite(t *testing.T) {
 	bf, err := InitBFile(ONE_BREACH_FILE) // hold the original for comparison (expected)
