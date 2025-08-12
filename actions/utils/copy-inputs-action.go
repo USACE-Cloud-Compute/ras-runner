c700fccb5a3e7d33da8c2f923d94409a46b945b6
Will Lehman <william.p.lehman@usace.army.mil>
2025-08-12 14:47:27 +0000

    adding ability to allow for paths other than 0 in copy inputs

--- actions/utils/copy-inputs-action.go
+++ actions/utils/copy-inputs-action.go
@@ -22,28 +22,30 @@ type CopyInputsAction struct {
 func (ca *CopyInputsAction) Run() error {
 	log.Println("Starting copy inputs action")
 	for _, ds := range ca.PluginManager.Inputs {
-		err := func() error {
-			source, err := ca.PluginManager.GetReader(cc.DataSourceOpInput{
-				DataSourceName: ds.Name,
-				PathKey:        "0",
-			})
+		for k := range ds.Paths {
+			err := func() error {
+				source, err := ca.PluginManager.GetReader(cc.DataSourceOpInput{
+					DataSourceName: ds.Name,
+					PathKey:        k,
+				})
+				if err != nil {
+					return err
+				}
+				defer source.Close()
+				destfile := fmt.Sprintf("%s/%s", actions.MODEL_DIR, filepath.Base(ds.Paths[k]))
+				log.Printf("Copying %s to %s\n", ds.Paths[k], destfile)
+				destination, err := os.Create(destfile)
+				if err != nil {
+					return err
+				}
+				defer destination.Close()
+				_, err = io.Copy(destination, source)
+				return err
+			}()
 			if err != nil {
+				log.Printf("Error fetching %s", ds.Paths[k])
 				return err
 			}
-			defer source.Close()
-			destfile := fmt.Sprintf("%s/%s", actions.MODEL_DIR, filepath.Base(ds.Paths["0"]))
-			log.Printf("Copying %s to %s\n", ds.Paths["0"], destfile)
-			destination, err := os.Create(destfile)
-			if err != nil {
-				return err
-			}
-			defer destination.Close()
-			_, err = io.Copy(destination, source)
-			return err
-		}()
-		if err != nil {
-			log.Printf("Error fetching %s", ds.Paths["0"])
-			return err
 		}
 	}
 	return nil
